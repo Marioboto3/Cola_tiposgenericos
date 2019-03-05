45a3fff29984e153769adcda7e28e25dbaa3543b
Marioboto3 <marioboto98@gmail.com>
2019-03-05 18:38:54 +0100

    Interfaz acabada.

--- src/Cola_tiposgenericos/Queue.java
+++ src/Cola_tiposgenericos/Queue.java
@@ -3,6 +3,9 @@ package Cola_tiposgenericos;
 import java.util.Collection;
 
 public interface Queue <E> extends Collection<E> {
-    
 
+    void push (E e);
+    E pop ();
+    int size();
+    
 }
