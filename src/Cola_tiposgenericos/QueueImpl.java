8a5d2d4ac6903f32a93fd44dcf7d8138a78684d8
Marioboto3 <marioboto98@gmail.com>
2019-03-05 18:46:41 +0100

    QueueImpl creado.

---
+++ src/Cola_tiposgenericos/QueueImpl.java
@@ -0,0 +1,19 @@
+package Cola_tiposgenericos;
+
+public class QueueImpl <E> implements Queue <E> {
+
+    @Override
+    public void push(E e) {
+
+    }
+
+    @Override
+    public E pop() {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+}
